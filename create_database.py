6e1bf3360a6a581d3bdfed797f23f68e807f8b75
mrcabo <dcgdiego@gmail.com>
2019-03-22 14:16:16 +0100

    fixed database generator.

--- create_database.py
+++ create_database.py
@@ -1,18 +1,16 @@
 import numpy as np
 
 
-def write_block(f, block, block_num, num_examples):
+def write_block(f, block, block_num):
     sInput = "I: {   1} "  # + "{}".format(0)
     sTarget = "T: {   1} "  # + "{}".format(0)
-    f.write("name: Block" + "{}".format(block_num) + " {}".format(num_examples) + "\n")
-    for _, x in enumerate(block):
+    for i, x in enumerate(block):
+        f.write("name: {" + "{}".format(i) + "_{}".format(block_num) + "} 2\n")
         f.write(sInput + "{}".format(x[0]) + '\n')
-        f.write(sTarget + "{}".format(0) + '\n')
+        f.write(sTarget + "{}".format(x[1]) + '\n')
         f.write(sInput + "{}".format(x[1]) + '\n')
-        f.write(sTarget + "{}".format(0) + '\n')
-        f.write(sInput + "{}".format(x[2]) + '\n')
         f.write(sTarget + "{}".format(x[2]) + '\n')
-    f.write(";\n")
+        f.write(";\n")
 
 
 if __name__ == "__main__":
@@ -56,12 +54,12 @@ if __name__ == "__main__":
     train_file = open(path, 'w')
     for i in range(6):
         np.random.shuffle(block)
-        write_block(train_file, block, i, 432)
+        write_block(train_file, block, i)
 
     np.random.shuffle(block_ungr)
-    write_block(train_file, block_ungr, "-ungrammatical", 864)
+    write_block(train_file, block_ungr, "6")
     np.random.shuffle(block)
-    write_block(train_file, block, "-recovery", 432)
+    write_block(train_file, block, "7")
 
     train_file.close()
 
