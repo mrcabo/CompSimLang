c40cae3137dcc2c894dbf8a250965f531ce83efd
mrcabo <dcgdiego@gmail.com>
2019-03-24 18:49:48 +0100

    latest changes

--- create_database.py
+++ create_database.py
@@ -13,11 +13,65 @@ def write_block(f, block, block_num):
         f.write(";\n")
 
 
+def write_block2w(f, block, block_num):
+    sInput = "I: {   1} "  # + "{}".format(0)
+    sTarget = "T: {   1} "  # + "{}".format(0)
+    for i, x in enumerate(block):
+        f.write("name: {" + "{}".format(i) + "_{}".format(block_num) + "} 3\n")
+        f.write(sInput + "{}".format(x[0]) + '\n')
+        f.write(sTarget + "{}".format(x[1]) + '\n')
+        f.write(sInput + "{}".format(x[1]) + '\n')
+        f.write(sTarget + "{}".format(x[2]) + '\n')
+        f.write(sInput + "{}".format(x[2]) + '\n')
+        f.write(sTarget + "{}".format(x[3]) + '\n')
+        f.write(";\n")
+
+
+def create_block_2w(n_pref, n_word, n_suf):
+    prefix_arr = np.arange(n_pref)
+    word1_arr = np.arange(n_pref, n_pref+n_word)
+    word2_arr = np.copy(word1_arr)
+    np.random.shuffle(word2_arr)
+    suffix_arr = np.arange(n_pref+n_word, n_pref+n_word+n_suf)
+
+    pref = np.reshape(np.repeat(prefix_arr[0], np.size(word1_arr)), (np.size(word1_arr), 1))
+    mid1 = np.reshape(word1_arr, (np.size(word1_arr), 1))
+    mid2 = np.reshape(word2_arr, (np.size(word2_arr), 1))
+    suf = np.reshape(np.repeat(suffix_arr[0], np.size(word2_arr)), (np.size(word2_arr), 1))
+    block = np.hstack((np.hstack((np.hstack((pref, mid1)), mid2)), suf))
+
+    for i in range(1, 3):
+        pref = np.reshape(np.repeat(prefix_arr[i], np.size(word1_arr)), (np.size(word1_arr), 1))
+        suf = np.reshape(np.repeat(suffix_arr[i], np.size(word1_arr)), (np.size(word1_arr), 1))
+        block = np.vstack((block, np.hstack((np.hstack((np.hstack((pref, mid1)), mid2)), suf))))
+
+    # Creating Ungrammatical
+    pref = np.reshape(np.repeat(prefix_arr[0], np.size(word1_arr)), (np.size(word1_arr), 1))
+    suf1 = np.reshape(np.repeat(suffix_arr[1], np.size(word1_arr)), (np.size(word1_arr), 1))
+    suf2 = np.reshape(np.repeat(suffix_arr[2], np.size(word1_arr)), (np.size(word1_arr), 1))
+    block_ungr = np.hstack((np.hstack((np.hstack((pref, mid1)), mid2)), suf1))
+    block_ungr = np.vstack((block_ungr, np.hstack((np.hstack((np.hstack((pref, mid1)), mid2)), suf2))))
+    pref = np.reshape(np.repeat(prefix_arr[1], np.size(word1_arr)), (np.size(word1_arr), 1))
+    suf1 = np.reshape(np.repeat(suffix_arr[0], np.size(word1_arr)), (np.size(word1_arr), 1))
+    suf2 = np.reshape(np.repeat(suffix_arr[2], np.size(word1_arr)), (np.size(word1_arr), 1))
+    block_ungr = np.vstack((block_ungr, np.hstack((np.hstack((np.hstack((pref, mid1)), mid2)), suf1))))
+    block_ungr = np.vstack((block_ungr, np.hstack((np.hstack((np.hstack((pref, mid1)), mid2)), suf2))))
+    pref = np.reshape(np.repeat(prefix_arr[2], np.size(word1_arr)), (np.size(word1_arr), 1))
+    suf1 = np.reshape(np.repeat(suffix_arr[0], np.size(word1_arr)), (np.size(word1_arr), 1))
+    suf2 = np.reshape(np.repeat(suffix_arr[1], np.size(word1_arr)), (np.size(word1_arr), 1))
+    block_ungr = np.vstack((block_ungr, np.hstack((np.hstack((np.hstack((pref, mid1)), mid2)), suf1))))
+    block_ungr = np.vstack((block_ungr, np.hstack((np.hstack((np.hstack((pref, mid1)), mid2)), suf2))))
+
+    return block, block_ungr
+
+
 if __name__ == "__main__":
-    np.random.seed(0)
+    # np.random.seed(0)
 
     train_filename = "q5_train.txt"
+    train2_filename = "q5_train2.txt"
     test_filename = "q5_test.txt"
+    test2_filename = "q5_test2.txt"
     num_prefixes = 3
     num_middle_words = 24
     num_suffixes = 3
@@ -58,13 +112,31 @@ if __name__ == "__main__":
         write_block(train_file, block, i)
 
     np.random.shuffle(block_ungr)
-    write_block(train_file, block_ungr, "6")
+    write_block(train_file, block_ungr, "6")  # Ungrammatical
     np.random.shuffle(block)
-    write_block(train_file, block, "7")
+    write_block(train_file, block, "7")  # Recovery
     train_file.close()
 
     # Make the test set
     train_file = open(test_filename, 'w')
     np.random.shuffle(block)
     write_block(train_file, block[0:12], 8)
+    train_file.close()
+
+    # Second part
+    block2w, block2w_ungr = create_block_2w(num_prefixes, num_middle_words, num_suffixes)
+    train_file = open(train2_filename, 'w')
+    for i in range(6):
+        np.random.shuffle(block2w)
+        write_block2w(train_file, block2w, i)
+    np.random.shuffle(block2w_ungr)
+    write_block2w(train_file, block2w_ungr, "6")  # Ungrammatical
+    np.random.shuffle(block2w)
+    write_block2w(train_file, block2w, "7")  # Recovery
+    train_file.close()
+
+    # Make the test set
+    train_file = open(test2_filename, 'w')
+    np.random.shuffle(block2w)
+    write_block2w(train_file, block2w[0:12], 8)
     train_file.close()
